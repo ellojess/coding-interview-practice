f398b658d140347201aff421cd21bee87e33108e
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-04-04 02:28:51 -0700

    implement pseudocode

--- LeetCode/remove-duplicates.py
+++ LeetCode/remove-duplicates.py
@@ -7,17 +7,14 @@ modifying the input array in-place with O(1) extra memory.
 
 Example: 
 Given nums = [1,1,2],
-
 Your function should return length = 2, with the first two 
 elements of nums being 1 and 2 respectively.
-
 It doesn't matter what you leave beyond the returned length.
 
 Clarification:
-
 Confused why the returned value is an integer but your answer is an array?
-
-Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+Note that the input array is passed in by reference, which means 
+modification to the input array will be known to the caller as well.
 
 Internally you can think of this:
 // nums is passed in by reference. (i.e., without making a copy)
@@ -28,6 +25,11 @@ for (int i = 0; i < len; i++) {
     print(nums[i]);
 }
 
+Pseudocode:
+- loop through array with two-pointer
+- one, that would keep track of the current element in the 
+original array and another one for just the unique elements.
+- if duplicate appears, bypass it until unique number is seen 
 '''
 
 class Solution:
