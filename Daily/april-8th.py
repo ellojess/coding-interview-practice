ffb351ebff76095cf72b07ec70be81fb002e891b
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-04-08 11:41:58 -0700

    add V1 from breakoutroom

---
+++ Daily/april-8th.py
@@ -0,0 +1,40 @@
+"""Given a list of n numbers, determine if it contains any duplicate numbers.
+Jessica Trinh
+
+Find duplicates in an array of numbers 
+
+Numbers are whole 
+Account for negatives
+Assume that the list of numbers is unsorted 
+Return Bool for whether or not a duplicate exists 
+And number that appears 2+ is a duplicate 
+
+Example: 
+
+[-3, 5, 6, -7, 8, 9, 10, 11, 5, 10, 2, -6] → True 
+
+Walkthrough: 
+
+Option A: 
+Sort the array
+As we’re looping through, if the value already exists in the set, return True 
+
+Option B: 
+Loop through the indexes and append each value to a set if the value doesn’t already exist in the set 
+Compare the sorted list to the set 
+Equal in length first 
+See if the values of list are equal
+If they are, then return true 
+If not then False """
+
+def checkDuplicates(list): 
+	sortedList = list.sort() 
+	setList = set() 
+	for item in sortedList: 
+		setList.append(item): 
+			if item in setList:
+				return True
+			else: 
+				return False 
+		
+# ** WHat’s the benefit of sorting when looking for duplicates ?? 
