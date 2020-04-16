75383263677d96c6ebc308a1c33ad1f49c23638c
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-04-15 23:38:03 -0700

    implement abbreviation function

--- Exercism/python/acronym/acronym.py
+++ Exercism/python/acronym/acronym.py
@@ -1,2 +1,15 @@
+"""
+- get the first letter of every word and capitalize it 
+- append each letter to a string and return the string 
+
+- handle spaces, - , _ , and other punctuations 
+
+"""
+
 def abbreviate(words):
-    pass
+  acronym = ""
+  wrds = words.replace('-', ' ').replace('_',' ').split() 
+  for word in wrds:
+    acronym += word[0].upper()
+
+  return acronym
