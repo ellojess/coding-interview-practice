7dd56a7e95228bf6547df2fe47ee43326018abf3
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-04-16 00:22:43 -0700

    implement matrix -- tests passed

--- Exercism/python/matrix/matrix.py
+++ Exercism/python/matrix/matrix.py
@@ -1,9 +1,17 @@
+"""
+- A list of the rows, reading each row left-to-right 
+  while moving top-to-bottom across the rows,
+- A list of the columns, reading each column top-to-bottom 
+  while moving from left-to-right.
+
+"""
+
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix = [list(map(int, x.split())) for x in matrix_string.split('\n')]
 
     def row(self, index):
-        pass
+        return self.matrix[index - 1]
 
     def column(self, index):
-        pass
+        return [x[index - 1] for x in self.matrix]
