28756d0ad1b26a3d4c62250b5546b978c3f9b5e7
Jess <jtrinh619@gmail.com>
2021-01-12 19:20:32 -0800

    [review] solution

--- Random/color_sort.py
+++ Random/color_sort.py
@@ -11,7 +11,25 @@ Extra Challenge:
 The boss is threatening to replace your job with a very efficient sorting robot, provided you don't perform better than it would. Luckily for you, the robot requires taking two passes over the array to sort the elements. You're not sure, but you think there might be a way to sort the elements that requires taking only one pass over the array, thus handily out-performing the robot.
 
 Original Platform:  https://leetcode.com/problems/sort-colors/
+
+notes: 
+- for every element in the list
+  - if the element is a 0, prepend it to the front 
+  - if element is 1, leave it alone 
+  - if 2, append to the end of the list 
 """
 
 def color_sort(nums):
-  pass
+
+  for index in range(len(nums)):
+    # if the number at the current index is 0 
+    if nums[index] == 0:
+      # remove num from index and add to front of the list 
+      nums.insert(0, nums.pop(index))
+    # if number at current index is 2
+    if nums[index] == 2: 
+      # append to the end of the list 
+      nums.append(nums.pop(index))
+  return nums
+
+  print(color_sort([0, 0, 2, 2, 1]))
